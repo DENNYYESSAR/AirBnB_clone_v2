ebc6eff17c68cdd9800fe3391d425a06a3aec241
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-19 15:33:03 +0300

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def python_text(text="is cool"):
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+def iamnumber(n):
     """
     Route that returns 'n is a number' only if n is an integer.
     """
@@ -52,7 +52,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def numbersandtemplate(n):
     """
     Route that returns an HTML page with 'Number: n' in H1 tag,
     only if n is an integer.
